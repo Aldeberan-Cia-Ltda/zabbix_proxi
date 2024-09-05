a3673dcb7f8990761f4b41d8c6fcacd505960650
Kerly <kerlyeliz19@hotmail.com>
2024-09-04 23:48:05 -0500

    Fixeds send zabbix data

--- services/sqs_event_service.py
+++ services/sqs_event_service.py
@@ -12,41 +12,45 @@ class SQSEventService:
             aws_secret_access_key=os.getenv('AWS_SECRET_ACCESS_KEY'),
             region_name=os.getenv('AWS_REGION')
         )
-        # Obtener la URL de la cola usando el nombre de la cola SQS
         queue_name = os.getenv('EVENTS_QUEUE_NAME')
         self.queue_url = self.sqs.get_queue_url(QueueName=queue_name)['QueueUrl']
         self.visibility_timeout = 30  # seconds
 
         # Instancia de ZabbixManager para enviar eventos a Zabbix
-        self.zabbix_client = zabbix_client
+        self.zabbix_manager = ZabbixManager(zabbix_client)
 
     def receive_messages(self):
         while True:
-            print('-------------------Estoy dentro de message')
             response = self.sqs.receive_message(
                 QueueUrl=self.queue_url,
                 MaxNumberOfMessages=10,
                 WaitTimeSeconds=20,  # Long polling
                 VisibilityTimeout=self.visibility_timeout
             )
-            print('-------------------El servicio de SQS esta activa con una respuesta', response)
+            print('-------------------El servicio de SQS está activo, respuesta:', response)
 
             messages = response.get('Messages', [])
             if messages:
                 for message in messages:
                     self.process_message(message)
-                    self.delete_message(message['ReceiptHandle'])
+                    
+                    # Verificar si 'ReceiptHandle' está presente antes de eliminar el mensaje
+                    receipt_handle = message.get('ReceiptHandle')
+                    if receipt_handle:
+                        self.delete_message(receipt_handle)
+                    else:
+                        print("No 'ReceiptHandle' found in the message")
             else:
-                time.sleep(5)  # Sleep briefly before polling again
+                time.sleep(5)  # Pausa breve antes de realizar la próxima consulta
 
     def process_message(self, message):
         print('-----------------------------------Estoy en los eventos---------------------------')
-        event = message['Body']
+        event = message.get('Body')
         print(f"Received event: {event}")
 
         # Enviar el evento a Zabbix
         event_key = "device.event"  # Clave para identificar el evento en Zabbix
-        self.zabbix_client.send_data(event,event_key, False)
+        self.zabbix_manager.send_data(event, event_key)
 
     def delete_message(self, receipt_handle):
         self.sqs.delete_message(
@@ -54,4 +58,3 @@ class SQSEventService:
             ReceiptHandle=receipt_handle
         )
         print("Message deleted successfully.")
-
